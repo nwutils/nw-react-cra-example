e5bc649a2c1200461380cbb9bb5d5d47fbe4c535
Brian Bothwell <sysrage@gmail.com>
2022-11-22 09:21:17 -0500

    postMessage test

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 'use strict';
 const url = require('node:url');
+const CryptoJS = require("crypto-js");
 
+// Build application URIs
 const baseUri = url.pathToFileURL(__dirname).toString();
-
 const interfaceUri = process.env.NWJS_START_URL
   ? process.env.NWJS_START_URL.trim()
   : `${baseUri}/build/`;
-
 const startUri = `${interfaceUri}/index.html`;
 
+// Listen for messages
+function handleMessage (event) {
+  const messageData = (typeof event.data === 'object') ? event.data : {};
+  if (!messageData.type) return;
+  switch (messageData.type) {
+    case 'cryptojs': {
+      const encryptedMFASecretHex = CryptoJS.AES.encrypt(
+        messageData.inputString,
+        'abcd1234567890'
+      ).toString();
+      console.log(`cryptojs result from the string "${messageData.inputString}": ${encryptedMFASecretHex}`);
+      event.source.postMessage({ type: 'result', result: encryptedMFASecretHex }, '*');
+      break;
+    }
+    default:
+      console.log(`Unknown event type: ${messageData.type}`);
+  }
+}
+window.addEventListener('message', handleMessage, false);
+
+// Open main window
 nw.Window.open(startUri);
--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [cryptoString, setCryptoString ] = useState(null);
+  useEffect(() => {
+    function handleMessage (event) {
+      if (event.data.type !== 'result') {
+        console.log(`Invalid response: ${event.data}`);
+        return;
+      }
+      setCryptoString(event.data.result);
+    }
+    window.addEventListener('message', handleMessage, { once: true });
+    window.opener.postMessage({ type: 'cryptojs', inputString: 'testing' }, '*');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Encrypted Text: {cryptoString}
         </p>
         <a
           className="App-link"
