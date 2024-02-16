68bf3e8f074075ac278e494e68808d0039076b78
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-02-16 13:29:34 -0500

    chore(eslint): remove react-refresh legacy config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,11 +13,4 @@ module.exports = {
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     settings: { react: { version: '18.2' } },
-    plugins: ['react-refresh'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-    },
 }
