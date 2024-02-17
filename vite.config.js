41a532ad34adbcd820f59347f3106160c214c2a3
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-02-16 19:53:24 -0500

    fix: build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    /* this is required so that Vite produces relative links to assets in the index.html*/
+    base: './',
     plugins: [react()],
     build: {
         outDir: './out/react'
